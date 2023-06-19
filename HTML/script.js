fb77c435b33287248c2b1634cdac043e779f6861
Sirigirisetty Krishna Vamsi <103875446+KrishnaVamsi-17@users.noreply.github.com>
2023-06-19 12:33:56 +0530

    Add files via upload

---
+++ HTML/script.js
@@ -0,0 +1,12 @@
+
+function cm()
+{
+    let input1 = document.querySelector('.inp');
+    let str1='';
+    str1 = input1.value;
+    if(str1.length<=0)
+    {
+        alert("Please enter a value");
+    }
+
+}
